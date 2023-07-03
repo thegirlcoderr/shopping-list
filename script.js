8de022b2e10bb33bc2fd5f56e303aa4d930d6955
thegirlcoderr <lynndy2015@gmail.com>
2023-07-03 18:12:16 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
+
+function addItem(e) {
+    e.preventDefault();
+
+    const newItem = itemInput.value;
+    //Input validation
+    if (newItem === '') {
+        alert('Hello! Please add an Item ');
+        return; } 
+   
+    
+    //create list item
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(newItem));
+    console.log(li)
+
+    const button = createButton('.remove-item btn-link text-red');
+    li.appendChild(button);
+    
+    itemList.appendChild(li);
+    itemInput.value === '';
+}
+
+function createButton(classes) {
+    const button = document.createElement('button')
+    button.className = classes;
+
+    const icon = createIcon('fa-solid fa-xmark')
+    button.appendChild(icon);
+    return button;
+
+    
+}
+
+function createIcon(classes) {
+    const icon = document.createElement('i')
+    icon.className = classes;
+    return icon;
+}
+
+
+
+
+
+
+
+
+
+
+//Event listeners
+itemForm.addEventListener('submit', addItem);
+itemInput.addEventListener();
+itemList.addEventListener();
